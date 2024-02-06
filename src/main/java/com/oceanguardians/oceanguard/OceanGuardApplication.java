f195ce1fb764cefa0eb01ee5e42c81395e31b3c8
sebav <seba.vidal.palacios@gmail.com>
2024-02-06 11:37:57 -0300

    BackEnd OceanGuard Proyect Integrador

---
+++ src/main/java/com/oceanguardians/oceanguard/OceanGuardApplication.java
@@ -0,0 +1,13 @@
+package com.oceanguardians.oceanguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oceanguardians/oceanguard/OceanGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceanguardians.oceanguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
