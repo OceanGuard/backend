d441447773c5065a15eb2f6232dff823adaac465
sebav <seba.vidal.palacios@gmail.com>
2024-02-14 17:22:40 -0300

    Se crea Query para Ingresar a datos en Veda desde nombreEspecie en Especies

--- src/main/java/com/oceanguardians/oceanguard/repository/VedaRepository.java
+++ src/main/java/com/oceanguardians/oceanguard/repository/VedaRepository.java
@@ -1,9 +1,17 @@
 package com.oceanguardians.oceanguard.repository;
 
+import com.oceanguardians.oceanguard.dto.VedaDTO;
 import com.oceanguardians.oceanguard.model.Veda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VedaRepository extends JpaRepository<Veda, Long> {
+
+    @Query("SELECT v FROM Veda v JOIN v.especieEnVeda e WHERE e.nombreEspecie = :nombreEspecies")
+    List<Veda> findByNombreEspecie(@Param("nombreEspecie") String nombreEspecie);
 }
