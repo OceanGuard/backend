ce2a0d1257d92325ad53dfdd9efefb861545920a
sebav <seba.vidal.palacios@gmail.com>
2024-02-16 15:02:04 -0300

    Se implementa función en la capa VedaService para buscar vedas por nombre de la especie

--- src/main/java/com/oceanguardians/oceanguard/services/VedaService.java
+++ src/main/java/com/oceanguardians/oceanguard/services/VedaService.java
@@ -1,4 +1,13 @@
 package com.oceanguardians.oceanguard.services;
 
+import com.oceanguardians.oceanguard.dto.VedaDTO;
+import com.oceanguardians.oceanguard.model.Veda;
+
+import java.util.List;
+
 public interface VedaService {
+
+    List<Veda> buscarPorNombreEspecie(String nombreEspecie);
+
+
 }
